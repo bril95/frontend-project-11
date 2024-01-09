7ad6fa8fc2bc91f2a3b5ddb01005f166829f68e3
bril95 <bril.vadim@mail.ru>
2024-01-09 22:03:27 +0200

    fix validate.js

--- src/validate.js
+++ src/validate.js
@@ -3,11 +3,7 @@ import * as yup from 'yup';
 const validate = (field, links) => {
   const schema = yup.string()
     .url('wrongLink')
-    .test('is-unique', 'doubleAdd', (value) => {
-      if (!links.includes(value)) {
-        return true;
-      } return false;
-    });
+    .notOneOf(links, 'doubleAdd');
 
   return schema.validate(field, { abortEarly: false })
     .then(() => field)
