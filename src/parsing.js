e4622b191dc9a268a32a425f01673329e579959b
bril95 <bril.vadim@mail.ru>
2024-01-20 12:35:00 +0200

    fix parsing.js

--- src/parsing.js
+++ src/parsing.js
@@ -1,31 +1,27 @@
 export default (data) => {
-  try {
-    const xmlString = data.contents;
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-    const parseError = xmlDoc.querySelector('parsererror');
-    if (parseError) {
-      return ('noValid');
-    }
-    const titleElement = xmlDoc.querySelector('channel title');
-    const title = titleElement.textContent;
-    const description = xmlDoc.querySelector('description').textContent;
-    const link = data.status.url;
-    const parsObj = {
-      title, description, items: [], link,
-    };
-    const items = xmlDoc.querySelectorAll('item');
-    items.forEach((item) => {
-      const itemTitle = item.querySelector('title').textContent;
-      const itemDescription = item.querySelector('description').textContent;
-      const itemLink = item.querySelector('link').textContent;
-      const itemObj = {
-        itemTitle, itemDescription, itemLink,
-      };
-      parsObj.items.push(itemObj);
-    });
-    return parsObj;
-  } catch (error) {
-    throw new Error('noValid');
+  const xmlString = data.contents;
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  const parseError = xmlDoc.querySelector('parsererror');
+  if (parseError) {
+    return ('noValid');
   }
+  const titleElement = xmlDoc.querySelector('channel title');
+  const title = titleElement.textContent;
+  const description = xmlDoc.querySelector('description').textContent;
+  const link = data.status.url;
+  const parsObj = {
+    title, description, items: [], link,
+  };
+  const items = xmlDoc.querySelectorAll('item');
+  items.forEach((item) => {
+    const itemTitle = item.querySelector('title').textContent;
+    const itemDescription = item.querySelector('description').textContent;
+    const itemLink = item.querySelector('link').textContent;
+    const itemObj = {
+      itemTitle, itemDescription, itemLink,
+    };
+    parsObj.items.push(itemObj);
+  });
+  return parsObj;
 };
