e8d80cf568232022da6459ea6b29db164d233435
bril95 <bril.vadim@mail.ru>
2024-01-20 15:02:34 +0200

    fix parsing.js

--- src/parsing.js
+++ src/parsing.js
@@ -1,16 +1,13 @@
 export default (data) => {
-  const xmlString = data.contents;
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  const xmlDoc = new DOMParser().parseFromString(data.contents, 'text/xml');
   const parseError = xmlDoc.querySelector('parsererror');
   if (parseError) {
     throw new Error('noValid');
   }
-  const titleElement = xmlDoc.querySelector('channel title');
-  const title = titleElement.textContent;
+  const title = xmlDoc.querySelector('channel title').textContent;
   const description = xmlDoc.querySelector('description').textContent;
   const link = data.status.url;
-  const parsObj = {
+  const parsedResult = {
     title, description, items: [], link,
   };
   const items = xmlDoc.querySelectorAll('item');
@@ -18,10 +15,10 @@ export default (data) => {
     const itemTitle = item.querySelector('title').textContent;
     const itemDescription = item.querySelector('description').textContent;
     const itemLink = item.querySelector('link').textContent;
-    const itemObj = {
+    const itemDetails = {
       itemTitle, itemDescription, itemLink,
     };
-    parsObj.items.push(itemObj);
+    parsedResult.items.push(itemDetails);
   });
-  return parsObj;
+  return parsedResult;
 };
