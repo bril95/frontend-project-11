8d705197afaf1b395b9b5f50ff945e10233c9eb9
bril95 <bril.vadim@mail.ru>
2024-02-03 12:28:53 +0200

    fix getResponse.js

--- src/utilities/getResponse.js
+++ src/utilities/getResponse.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default (link) => axios({
   method: 'get',
-  url: `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(new URL(link))}`,
+  url: `https://allorigins.hexlet.app/get?disableCache=true&url=${new URL(link)}`,
 })
   .catch((error) => error);
